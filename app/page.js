f125309490f01ed14cd8f0989a5a75d0a1dba36d
Madhusudan Kulkarni <96856144+madhusudan-kulkarni@users.noreply.github.com>
2024-06-13 08:53:06 +0530

    feat: Add ObjectDetection component for object detection feature

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
+import ObjectDetection from "@/components/ObjectDetection";
 import React from "react";
 
 const Home = () => {
   return (
-    <main className="flex flex-col items-center min-h-screen p-8">
+    <main className="flex flex-col items-center min-h-screen px-8 py-2">
       <h1 className="text-3xl font-extrabold tracking-tighter text-center md:text-6xl lg:text:8xl md:px-6 gradient-title">
         Object Detection
       </h1>
+      <ObjectDetection />
     </main>
   );
 };
---
+++ components/ObjectDetection.js
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useRef } from "react";
+
+const ObjectDetection = () => {
+  return (
+    <div className="mt-8">
+      <div className="flex relative justify-center items-center gradient p-1.5 rounded-md">
+        {/* webcam */}
+        {/* canvas */}
+      </div>
+    </div>
+  );
+};
+
+export default ObjectDetection;
