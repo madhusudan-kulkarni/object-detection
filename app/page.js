ad99ee4059fb355e52b7a27a98bc19c2094bb3c2
Madhusudan Kulkarni <96856144+madhusudan-kulkarni@users.noreply.github.com>
2024-06-13 08:33:58 +0530

    feat: Update landing page title with gradient effect

--- app/page.js
+++ app/page.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-4xl">Object Detection</h1>
-    </div>
+    <main className="flex flex-col items-center min-h-screen p-8">
+      <h1 className="text-3xl font-extrabold tracking-tighter text-center md:text-6xl lg:text:8xl md:px-6 gradient-title">
+        Object Detection
+      </h1>
+    </main>
   );
 };
 
